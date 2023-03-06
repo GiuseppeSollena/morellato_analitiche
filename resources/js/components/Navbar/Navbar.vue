75b71a671fc0d5fe589faea656b60dd71151df96
Cristian Chirivi <c.chirivi@esis-italia.com>
2023-03-06 10:46:21 +0100

    InizioFrontend: push dynamics link on navbar + icons

--- resources/js/components/Navbar/Navbar.vue
+++ resources/js/components/Navbar/Navbar.vue
@@ -4,68 +4,53 @@
 
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <RouterLink 
-      to="/" 
-        class="navbar-item" href="/">
-      HOME
-    </RouterLink>
+        <div class="navbar-brand">
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      
-      <RouterLink 
-        to="statistics" 
-          class="navbar-item">
-        Statistiche
-      </RouterLink>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            prova1
-          </a>
-          <a class="navbar-item">
-            prova2
-          </a>
-          <a class="navbar-item">
-            prova3
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            prova4
-          </a>
         </div>
-      </div>
-    </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+
+                <li to="statistics" class="navbar-item"  v-for="(elem, index) in navElemList" v-bind:key="index">
+                    <a class="nav-link" :href=elem.link >{{elem.label}}</a>
+                </li>
+
+
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-</nav>
+    </nav>
 </template>
 
-
+<script>
+    export default {
+        data() {
+            return {
+                navElemList: [{ label: "HOME", link: "/" },
+                    { label: "Statistiche", link: "/statistics" },
+                ],
+                logoutRoute: "/logout"
+            };
+        }
+};
+</script>
 
 <style src="./Navbar.css" scoped></style>
