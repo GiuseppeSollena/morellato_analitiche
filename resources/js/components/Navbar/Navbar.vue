30956dbbe11b6d4e355f29b1be1d81c87b8681d7
Giuseppe Sollena <92548016+GiuseppeSollena@users.noreply.github.com>
2023-03-07 16:30:57 +0100

    MB-3

--- resources/js/components/Navbar/Navbar.vue
+++ resources/js/components/Navbar/Navbar.vue
@@ -1,41 +1,50 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-navbar">
         <div class="container-fluid">
-            <div class="div-logo d-flex justify-content-center align-items-center">
-                <a href="https://www.morellato.com/">
-                    <img class="img-logo" src="https://cdn.morellato.com/media/images/logos/logo_morellato_footer_venice.png" alt="">
-                </a>
+
+            <div class="blocco-logo">
+                <div class="div-logo d-flex ">
+                    <a href="https://www.morellato.com/">
+                        <img class="img-logo"
+                            src="https://cdn.morellato.com/media/images/logos/logo_morellato_footer_venice.png" alt="">
+                    </a>
+                </div>
             </div>
-            <!-- <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button> -->
-            <div class="collapse navbar-collapse blocco-section" id="navbarNavAltMarkup">
-                <div class="navbar-nav bg-navbar" v-for="(elem, index) in navElemList" v-bind:key="index">
-                    <!-- <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Statistics</a>
-                    <a class="nav-link" href="#">Altro</a> 
-                    <div class="position-absolute top-5 end-0 btnLogin"><login-btn></login-btn></div>   -->
-                    <a class="nav-link" :href=elem.link >{{ elem.label }}</a>
-                    <div class="position-absolute top-5 end-0 btnLogin"><login-btn></login-btn></div> 
+
+            <button class="navbar-toggler btn-menu" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span><img class="img-menu" src="../../../img/icon-menu.png" alt=""></span>
+            </button>
+
+            <div class="collapse navbar-collapse blocco-section-btn " id="navbarNavAltMarkup">
+                <div class="blocco-section d-flex">
+                    <div class="navbar-nav" v-for="(elem, index) in navElemList" v-bind:key="index">
+                        <a class="nav-link" :href=elem.link> {{ elem.label }} </a>
+                    </div>
+                </div>
+                <div class="blocco-btnLogin">
+                        <login-btn></login-btn>
                 </div>
             </div>
         </div>
+
     </nav>
 </template>
 
 <script>
-    export default{
-        data(){
-            return{
-                navElemList:[
-                    { label: "HOME", link: "/"},
-                    { label: "STATISTICS", link: "/statistics"},
+export default {
+    data() {
+        return {
+            navElemList: [
+                { label: "HOME", link: "/" },
+                { label: "STATISTICS", link: "/statistics" },
 
-                ]
-            };
-        }
+            ]
+        };
     }
+}
+
 </script>
 
 <style src="./Navbar.css" scoped></style>
